053fd6fa2de0943579a75bdc4781bfc88f88ad7b
pbenito2016 <64688143+pbenito2016@users.noreply.github.com>
2020-05-02 15:03:16 +0200

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'cms.apps.CmsConfig',
+'cms.apps.CmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
