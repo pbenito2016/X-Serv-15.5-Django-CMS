13ae699179a4f7e68ed878324ea287b814815420
p.benito.2016 <you@example.com>
2020-05-02 15:33:08 +0200

    añado url cms

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+	path('cms/', include('cms.urls')),
     path('calc/', include('calc.urls')),
     path('admin/', admin.site.urls),
 ]
