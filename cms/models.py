efe573284ef74600c49c80875d2861f9f3a47b5f
p.benito.2016 <you@example.com>
2020-05-02 14:01:50 +0200

    Añado comentario y contenido

--- cms/models.py
+++ cms/models.py
@@ -5,3 +5,14 @@ from django.db import models
 class Pages(models.Model):
     name = models.CharField(max_length=32)
     page = models.TextField()
+    
+class Contenido(models.Model):
+    clave = models.CharField(max_length=64)
+    valor = models.TextField()
+
+class Comentario(models.Model):
+    contenido = models.ForeignKey(Contenido, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    cuerpo = models.TextField(blank=False)
+    fecha = models.DateTimeField('publicado')
+
