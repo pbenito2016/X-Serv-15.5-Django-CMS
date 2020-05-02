f768462230b3c74addeed50e4f1c01b75b68aed6
p.benito.2016 <you@example.com>
2020-05-02 14:53:24 +0200

    .

--- proyecto/wsgi.py
+++ proyecto/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for myproject project.
+WSGI config for proyecto project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto.settings')
+
 application = get_wsgi_application()
